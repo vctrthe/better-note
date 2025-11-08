4a5704c4cf13d6a342101e0c844ce081181345f8
vctrthe <victorthe123@gmail.com>
2025-11-08 19:45:22 +0700

    Remove unused import from preload script

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,5 +1,4 @@
 import { contextBridge } from 'electron'
-import { electronAPI } from '@electron-toolkit/preload'
 
 if (!process.contextIsolated) {
   throw new Error('contextIsolation must be enabled in the BrowserWindow')
